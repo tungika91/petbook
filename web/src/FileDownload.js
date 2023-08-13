09a2bdc534b4a01d62d740d443eca73bb223af57
tungika91 <40808780+tungika91@users.noreply.github.com>
2023-08-13 09:56:13 +0800

    fix bug in download medical record

--- web/src/FileDownload.js
+++ web/src/FileDownload.js
@@ -1,26 +1,29 @@
 import { useContext } from "react"
-import { useState } from "react";
 import DataContext from './context/DataContext';
 import api from './api/posts';
 
 const FileDownload = ({ record_id, pet_id }) => {
     const { userID, auth } = useContext(DataContext);
-    const [url, setUrl] = useState('');
 
     const handleDownload = async () => {
-        const response = await api.get(`${userID}/pets/${pet_id}/medical/${record_id}/download`, {
-            headers: {
-                'x-access-token': auth.accessToken
-            }
-        });
-        console.log(response.data);
-        setUrl(response.data);
-        // Open in new tab in browser
-        const pdfWindow = window.open();
-        pdfWindow.location.href = url;  
-        setUrl('')
+        try {
+                await api.get(`${userID}/pets/${pet_id}/medical/${record_id}/download`, {
+                headers: {
+                    'x-access-token': auth.accessToken
+                }
+            })
+            .then((response) => {
+                // Open in new tab in browser
+                window.open(response.data);        
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+        catch (error) {
+            return { error };
+        }
     }
-
     return (
         <>
             <button onClick={handleDownload}> Download </button>
